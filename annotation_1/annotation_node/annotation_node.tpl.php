018370fd2a69157593a0f1cddf0bc6a95dd416b9
derhasi <derhaside@gmail.com>
2009-09-20 19:07:45 +0200

    Current status (changed for cs)

---
+++ annotation_1/annotation_node/annotation_node.tpl.php
@@ -0,0 +1,4 @@
+<?php
+// $Id$
+?>
+<div><?php print l($node->title, 'node/'. $node->nid); ?></div>
---
+++ annotation_1/annotation_user/annotation_user.tpl.php
@@ -0,0 +1,5 @@
+<?php
+// $Id$
+?>
+
+<?php print l($annotation_user->name, 'user/'. $annotation_user->uid); ?>
