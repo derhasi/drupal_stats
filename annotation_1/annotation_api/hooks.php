ea2317c30f5e1fdd6a2513e10329d5deac8a6604
derhasi <derhaside@gmail.com>
2009-09-20 15:47:38 +0200

    current state

--- annotation_1/annotation_api/hooks.php
+++ annotation_1/annotation_api/hooks.php
@@ -31,15 +31,14 @@
  *      - view - the function that is used to view the annotation
  *      - settings_form - form function to add additional settings
  *      - use hook_form_annotation_form_alter() to alter the input form
+ *      - fit
  *   * type: scope
  *      - title - Human readable name for the plugin
- *
  */
-function hook_annotation_info($type) {
-  $info = array();
-  switch ($type) {
-    case 'scope':
-      $info['node'] = array(
+function hook_annotation_info() {
+  return array(
+    'scope' => array(
+      'node' => array(
         'title' => t('Node'),
         'help' => t('Provides nodes as scope for an annotation.'),
         'relation' => array(
@@ -48,9 +47,10 @@ function hook_annotation_info($type) {
         ),
         'settings_form' => '_annotation_node_options',
         'fit' => '', //Function that defines, if this scope fits to the current call or not
-      );
-    case 'content':
-      $info['example'] = array(
+      ),
+    ),
+    'content' => array(
+      'example' => array(
         'title' => t('Example'),
         'help' => t('Connects example to annotations'),
         'relation' => array(
@@ -61,18 +61,18 @@ function hook_annotation_info($type) {
         'view' => 'example_content_view',
         'form' => 'annotation_example_content_form', // ($annotation)provides the form element for content_id
         'fit' => '', // function that indicates if the content may be i used in the current call.
-      );
-      break;
-    case 'annotation':
-      $info['point'] = array(
+        'default object' => '', // function that will build default values for new $annotation objects
+      ),
+    ),
+    'annotation' => array(
+      'point' => array(
         'title' => t('Point'),
         'view' => 'annotation_example_annotation_view',// ($annotation, $content)
         //'form_alter' => 'annotation_example_annotation_formalter', // (&$form, $annotation)is the first function to alter the default form.
         'fit' => '', // callback function that indicates whether this annotation type may be used on the current context.
-      );
-      break;
-  }
-  return $info;
+      ),
+    ),
+  );
 }
 
 /**
---
+++ annotation_cck/README.txt
@@ -0,0 +1,7 @@
+# $Id$
+
+Module for proof of concept for annotations.
+
+Components:
+
+* CCK field for storage matters
